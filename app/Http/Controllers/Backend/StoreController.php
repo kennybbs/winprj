a3dc739d52451a16eeda84fb0aa6be6d5845ac81
kennybbs <kennybbs@gmail.com>
2019-03-20 22:59:48 +0800

    tmp commit

---
+++ app/Http/Controllers/Backend/StoreController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Store;
+
+class StoreController extends Controller
+{
+    //
+    public function edit()
+    {
+        // ...
+    }
+    
+    public function update(Request $request)
+    {
+        // ...
+    }
+}
---
+++ app/Http/Controllers/Backend/WebsiteController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Website;
+
+class WebsiteController extends Controller
+{
+    //
+    public function edit()
+    {
+        // ...
+    }
+
+    public function update(Request $request)
+    {
+        // ...
+    }
+}
